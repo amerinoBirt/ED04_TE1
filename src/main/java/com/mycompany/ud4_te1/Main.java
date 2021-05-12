a981152bf7fd81d0210139c0eb1bb67b64c33371
Alvaro Merino Maestre <amerino@birt.eus>
2021-05-12 10:07:29 +0200

    Version 2. Refactorizacion con introduce method

--- src/main/java/com/mycompany/ud4_te1/Main.java
+++ src/main/java/com/mycompany/ud4_te1/Main.java
@@ -8,9 +8,13 @@ public class Main {
         double saldoActual;
 
         cuentaTrabajo = new CCuenta("Antonio López","1000-2365-85-1230456789", 2500,0);
+        operativaCuenta(cuentaTrabajo);
+    }
+
+    private static void operativaCuenta(CCuenta cuentaTrabajo) {
+        double saldoActual;
         saldoActual = cuentaTrabajo.estado();
         System.out.println("El saldo actual es " + saldoActual);
-
         try {
             System.out.println("Cargo en cuenta");
             cuentaTrabajo.retirar(2300);
@@ -18,7 +22,6 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
-        
         try {
             System.out.println("Abono en cuenta");
             cuentaTrabajo.ingresar(695);
